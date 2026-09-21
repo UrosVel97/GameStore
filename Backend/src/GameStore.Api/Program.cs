20b8ab2f3ba014e21bbb73edfd5faa7bda823b40
UrosVel97 <velickovic97@gmail.com>
2026-09-21 13:21:28 +0200

    Add initial database migration and update connection settings for SQLite

--- Backend/src/GameStore.Api/Program.cs
+++ Backend/src/GameStore.Api/Program.cs
@@ -1,13 +1,21 @@
 using GameStore.Api.Data;
 using GameStore.Api.Features.Genres;
 using GameStore.Api.Features.Games;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("GameStore");
+
+builder.Services.AddSqlite<GameStoreContext>(connectionString);
 
 builder.Services.AddValidation();
 
 builder.Services.AddSingleton<GameStoreData>();
 builder.Services.AddTransient<GameDataLogger>();
+
+
+
 
 var app = builder.Build();
 
